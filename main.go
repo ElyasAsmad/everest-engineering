8a834a01ec4c541bb51f9727a2a2e58aa2da9876
Elyas Asmad <elyasasmadz@gmail.com>
2026-03-23 15:38:32 +0800

    add main logic for processing offers and shipments with input validation

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+
+	"github.com/ElyasAsmad/everestengineering2/internal/model"
+	"github.com/ElyasAsmad/everestengineering2/pkg/combinator"
+	l "github.com/ElyasAsmad/everestengineering2/pkg/logger"
+	"github.com/ElyasAsmad/everestengineering2/pkg/parser"
+	s "github.com/ElyasAsmad/everestengineering2/pkg/shipper"
+)
+
+func main() {
+	logger := l.NewLogger()
+
+	// TODO: maybe make the file name passable via arg
+	parsed, err := parser.ParseOffersCSV("offers.csv")
+	if err != nil {
+		logger.Error("Failed to parse CSV file: %v", err)
+		return
+	}
+
+	offers := parsed.ConvertToOffer()
+
+	logger.Debugf("Parsed Offers: %v", offers)
+
+	// 1st scan: base cost, number of packages
+	var baseCost float64
+	var noOfPackages int
+
+	// 2nd scan: no of vehicles, max speed, max load
+	var noOfVehicles int
+	var maxSpeed float64
+	var maxLoad float64
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if scanner.Scan() {
+		line := scanner.Text()
+
+		// read base cost, no of packages
+		n, err := fmt.Sscanf(line, "%f %d", &baseCost, &noOfPackages)
+
+		if err != nil || n != 2 {
+			logger.Error("Invalid input format. Expected: <baseCost> <noOfPackages>")
+			os.Exit(1)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error("Error reading base cost and number of packages:", err)
+		os.Exit(1)
+	}
+
+	logger.Debugf("Base Cost: %f", baseCost)
+	logger.Debugf("Number of Packages: %d", noOfPackages)
+
+	packages := make([]model.Package, noOfPackages)
+
+	for i := 0; i < noOfPackages; i++ {
+
+		if scanner.Scan() {
+			line := scanner.Text()
+
+			var id string
+			var weight float64
+			var distance float64
+			var offerCode string
+
+			// package details (id, weight, distance, offer code)
+			n, err := fmt.Sscanf(line, "%s %f %f %s", &id, &weight, &distance, &offerCode)
+
+			if err != nil || n != 4 {
+				logger.Error("Invalid package details format. Expected: <id> <weight> <distance> <offerCode>")
+				os.Exit(1)
+			}
+
+			logger.Debugf("Package ID: %s, Weight: %f, Distance: %f, Offer Code: %s", id, weight, distance, offerCode)
+
+			packages[i] = model.Package{
+				ID:         id,
+				WeightKg:   weight,
+				DistanceKm: distance,
+				OfferCode:  offerCode,
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			logger.Error("Error reading package details:", err)
+			os.Exit(1)
+		}
+	}
+
+	if scanner.Scan() {
+		line := scanner.Text()
+
+		n, err := fmt.Sscanf(line, "%d %f %f", &noOfVehicles, &maxSpeed, &maxLoad)
+
+		if err != nil || n != 3 {
+			logger.Error("Invalid vehicle details format. Expected: <noOfVehicles> <maxSpeed> <maxLoad>")
+			os.Exit(1)
+		}
+
+		logger.Debugf("Number of Vehicles: %d, Max Speed: %f, Max Load: %f", noOfVehicles, maxSpeed, maxLoad)
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error("Error reading vehicle details:", err)
+		os.Exit(1)
+	}
+
+	// fleet manager
+	shipper := s.NewShipper(noOfVehicles, maxSpeed)
+
+	// start loop
+	for len(packages) > 0 {
+
+		// generate combinations
+		combinations := combinator.GenerateCombinations(packages, maxLoad)
+
+		// get optimal shipment from generated combinations
+		op, err := combinator.GetOptimalShipment(combinations)
+
+		if err != nil {
+			logger.Error("Error getting optimal shipment:", err)
+			os.Exit(1)
+		}
+
+		logger.Debugf("Optimal Shipment: %v, Total Weight: %f", op.Packages, op.TotalWeight)
+
+		shipper.ProcessShipment(op)
+
+		packages = slices.DeleteFunc(packages, func(p model.Package) bool {
+			for _, shippedPkg := range op.Packages {
+				if p.ID == shippedPkg.ID {
+					return true
+				}
+			}
+			return false
+		})
+
+		for _, pkg := range packages {
+			logger.Debugf("Remaining Package: %s", pkg.ID)
+		}
+	}
+}
